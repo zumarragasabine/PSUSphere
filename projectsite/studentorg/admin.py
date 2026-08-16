b2a32bbed209498b9017da198096a26be2d4896a
isobelsab <zumarragsasabine@gmail.com>
2026-08-16 16:05:34 +0800

    models and admin working properly

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import College, Program, Organization, Student, OrgMember
 
-# Register your models here.
+admin.site.register(College)
+admin.site.register(Program)
+admin.site.register(Organization)
+admin.site.register(Student)
+admin.site.register(OrgMember)
