ec7be000001b0c2949e48cfefb4cd900fceb888b
Yelwaduu <yhue112x@gmail.com>
2026-08-24 19:51:53 +0800

    Sucessful Set up in BootStrap

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from studentorg import views
+from studentorg.views import HomePageView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", HomePageView.as_view(), name="home"),
 ]
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from studentorg.models import Organization
 
-# Create your views here.
+
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = "home"
+    template_name = "home.html"
+
