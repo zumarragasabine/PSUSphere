c2f3ae4211cfe1f063a3242ddfb87683cec87790
isobelsab <zumarragsasabine@gmail.com>
2026-09-06 19:19:41 +0800

    add total_students and students_joined_this_year to home context

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -11,6 +11,22 @@ class HomePageView(ListView):
     model = Organization
     context_object_name = "home"
     template_name = "home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+        return context
 
 class OrganizationList(ListView):
     model = Organization
